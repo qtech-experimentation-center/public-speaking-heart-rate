81a328c3b0bc39f9768b9ed8c9f0700418be04fa
amesiastw <amesias@infoarmor.com>
2020-10-15 22:19:41 -0500

    SAVINGTOROOM: [Alejo] removes unused user model

--- app/src/main/java/com/example/heartbeatreader/AppDatabase.java
+++ app/src/main/java/com/example/heartbeatreader/AppDatabase.java
@@ -3,8 +3,7 @@ package com.example.heartbeatreader;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {User.class, Sample.class}, version = 2)
+@Database(entities = {Sample.class}, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
-    public abstract UserDao userDao();
     public abstract SampleDao sampleDao();
 }
